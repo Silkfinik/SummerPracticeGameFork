32523394096f40577881e20521c6cfb3c1dde16a
silkfinik <wottankiazzaa@gmail.com>
2024-06-25 14:12:13 +0300

    fix: previous commit copy

--- main.py
+++ main.py
@@ -1,5 +1,69 @@
 import pygame
+import sys
+from sprites import Player, load_images
 
-print('lksaklasdk')
+# Инициализация Pygame
+pygame.init()
 
-print('Kirill test pull request')
+# Размеры окна
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.display.set_caption("Анимация спрайтов")
+
+# Основной цикл
+clock = pygame.time.Clock()
+
+# Путь к директории с анимациями спрайтов
+sprite_dir = 'sprites'
+
+# Загрузка изображений спрайта
+animations = load_images(sprite_dir)
+
+# Создание игрока
+player = Player(animations, 100, screen_height - 100)
+
+# Группа спрайтов
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+# Загрузка изображения фона
+background_image = pygame.image.load('background.jpg')
+background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
+
+# Основной игровой цикл
+running = True
+while running:
+    dt = clock.tick(60) / 1000  # Время, прошедшее с последнего кадра (в секундах)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Обработка нажатий клавиш
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        player.move_left()
+    elif keys[pygame.K_RIGHT]:
+        player.move_right()
+    else:
+        if player.on_ground:
+            player.change_animation("idle")
+
+    if keys[pygame.K_UP]:
+        player.jump()
+
+    # Обновление всех спрайтов
+    all_sprites.update(dt)
+
+    # Отрисовка фона
+    screen.blit(background_image, (0, 0))
+
+    # Отрисовка всех спрайтов
+    all_sprites.draw(screen)
+
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
---
+++ sprites.py
@@ -0,0 +1,83 @@
+import pygame
+import os
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, animations, x, y):
+        super().__init__()
+        self.animations = animations
+        self.current_animation = "idle"
+        self.images = self.animations[self.current_animation]
+        self.current_image = 0
+        self.image = self.images[self.current_image]
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.velocity = pygame.math.Vector2(0, 0)
+        self.gravity = 0.5
+        self.jump_power = -10
+        self.on_ground = False
+
+        self.animation_speed = 0.07  # скорость анимации в секундах на кадр
+        self.animation_timer = 0
+
+    def update(self, dt):
+        # Обновление анимации
+        self.animation_timer += dt
+        if self.animation_timer >= self.animation_speed:
+            self.animation_timer = 0
+            self.current_image = (self.current_image + 1) % len(self.images)
+            self.image = self.images[self.current_image]
+
+        # Обновление положения
+        self.velocity.y += self.gravity
+        self.rect.y += self.velocity.y
+
+        # Обработка столкновения с землей
+        if self.rect.bottom >= 600:  # screen_height должно быть передано или жестко закодировано
+            self.rect.bottom = 600
+            self.velocity.y = 0
+            self.on_ground = True
+            if self.current_animation == "jump":
+                self.change_animation("idle")
+
+        # Ограничение по горизонтали
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > 800:  # screen_width должно быть передано или жестко закодировано
+            self.rect.right = 800
+
+    def move_left(self):
+        self.rect.x -= 5
+        if self.on_ground:
+            self.change_animation("walk_left")
+
+    def move_right(self):
+        self.rect.x += 5
+        if self.on_ground:
+            self.change_animation("walk_right")
+
+    def jump(self):
+        if self.on_ground:
+            self.velocity.y = self.jump_power
+            self.on_ground = False
+            self.change_animation("jump")
+
+    def change_animation(self, animation):
+        if self.current_animation != animation:
+            self.current_animation = animation
+            self.images = self.animations[self.current_animation]
+            self.current_image = 0
+            self.animation_timer = 0
+            self.image = self.images[self.current_image]
+
+
+def load_images(sprite_dir):
+    animations = {}
+    for animation in os.listdir(sprite_dir):
+        animation_path = os.path.join(sprite_dir, animation)
+        if os.path.isdir(animation_path):
+            frames = []
+            for img_file in sorted(os.listdir(animation_path)):
+                img_path = os.path.join(animation_path, img_file)
+                frames.append(pygame.image.load(img_path).convert_alpha())
+            animations[animation] = frames
+    return animations
